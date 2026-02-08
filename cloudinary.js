9dd261fbf65b35d789620a5baa8dfed5240680e5
AhmedRaza186 <sallumoon650@gmail.com>
2026-02-08 21:53:23 +0500

    ..

--- cloudinary.js
+++ cloudinary.js
@@ -2,8 +2,7 @@ let apiKey = 297292193559886
 let apiSecret = 'ySa4BjLMWh2UqyI9HrirRGHDwF8'
 
 
-// cloudName
-// dddcijrz6
+
 const url = 'https://api.cloudinary.com/v1_1/dddcijrz6/image/upload';
 export async function uploadImg(formData){
     let response = await fetch(url, {
